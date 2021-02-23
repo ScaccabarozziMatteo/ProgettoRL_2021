8c5594c563dbc2537834a14a72fa89647b311a98
ScaccabarozziMatteo <barozzi966@gmail.com>
2021-02-24 00:12:39 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Progetto di Reti Logiche - [A.A. 2020/2021]
+ 
+Prof. Palermo Gianluca
+
+## Descrizione del progetto
+Lo scopo del progetto è sviluppare .... Sfruttando l'IDE Vivado ...
+
+## Implementazione
+Abbiamo deciso di utilizzare...
+
+## Sviluppatori
+[Scaccabarozzi Matteo](https://github.com/ScaccabarozziMatteo)
+
+[Alexander Tenemaya](https://github.com/AlexT1999)
---
+++ project_VHDL.vhd
@@ -0,0 +1,28 @@
+----------------------------------------------------------
+-- Progetto di Reti Logiche
+-- SCACCABAROZZI MATTEO - ALEXANDER TENEMAYA
+--
+-- A.A. 2020/2021
+-- Prof. Palermo Gianluca
+-- 
+----------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity project_reti_logiche is
+    port (
+      i_clk : in std_logic;
+      i_rst : in std_logic;
+      i_start : in std_logic;
+      i_data : in std_logic_vector(7 downto 0);
+      o_address : out std_logic_vector(15 downto 0);
+      o_done : out std_logic;
+      o_en : out std_logic;
+      o_we : out std_logic;
+      o_data : out std_logic_vector (7 downto 0)
+    );
+end project_reti_logiche;
